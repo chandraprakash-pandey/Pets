bf0715cd574f33e2b5038f49db16de3d2a755c03
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2024-09-14 22:16:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Components/header/header'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {link, NavLink} from 'react-router-dom' // we use 'link' instead of 'a' tag  cuz agar a tag use karenge toh page refresh hoga, but link use karenge toh page refresh nahi hoga. Link mai hum href k jaga pe "to" use krte hai.
+
+function Header() {
+    return (
+        <header className='lg:w-full '>
+            sa
+        </header>
+    )
+}
+
+export default Header
