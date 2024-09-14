8cc2a0827f91c4e52a372ea8df7c0f62e5ddab0f
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2024-09-14 22:42:20 +0530

    Updated

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 
 function Header() {
     return (
-        <header className='lg:w-full '>
-            sa
+        <header className='lg:w-full text-[#212548] lg:h-16 bg-slate-100 lg:border-b-4 lg:border-b-[#212548] rounded-lg'>
+            <div>
+                <div>
+                    <img src="./images/paw.png" alt="" className='w-10 h-10' />
+                    <img src="./" alt="" />
+                    <a href="./"></a>
+                </div>
+            </div>
         </header>
     )
 }
